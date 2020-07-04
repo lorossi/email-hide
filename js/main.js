8bd6dcc9f140b044f69aac846a06f6524911c779
lorossi <lo.rossi@live.com>
2020-07-04 20:59:35 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 function encode(text, iterations = 1) {
+  // encodes clear text into Base64 through multiple iterations
   let encoded = text;
   for (let i = 0; i < iterations; i++) {
-    encoded = btoa(encoded);
+    encoded = btoa(encoded); // text to Base64
   }
 
   return encoded;
 }
 
 function randHexString(len) {
+  // generates random hex string of set length
     let hex_string = "_0x";
     for (let i = 0; i < len; ++i) {
         hex_string += (Math.floor(Math.random() * 16)).toString(16);
@@ -16,7 +18,10 @@ function randHexString(len) {
 }
 
 function generateCode(encoded_container, encoded_email, iterations = 1, obfuscate = false, hide_variables = false, strip_newlines = false) {
+  // generates js code from templates
   let container_variable, email_variable, function_name, index_variable;
+
+  // hide variables names
   if (hide_variables) {
     container_variable = randHexString(6);
     email_variable = randHexString(6);
@@ -31,11 +36,13 @@ function generateCode(encoded_container, encoded_email, iterations = 1, obfuscat
 
   let js_code = `window.onload=function ${function_name}(){\n\tlet ${container_variable}="${encoded_container}";\n\tlet ${email_variable}="${encoded_email}";\n\tfor(let ${index_variable}=0;${index_variable}<${iterations};${index_variable}++){\n\t\t${container_variable}=atob(${container_variable});\n\t\t${email_variable}=atob(${email_variable});\n\t}\n\tdocument.querySelector(${container_variable}).innerHTML=${email_variable};\n}`
 
+  // strip newlines and tabs
   if (strip_newlines) {
     js_code = js_code.replace(/\n/g, '');
     js_code = js_code.replace(/\t/g, '');
   }
 
+  // obfuscate - very aggressively
   if (obfuscate) {
     options = {
       deadCodeInjection: true,
@@ -59,17 +66,22 @@ function generateDowload(js_code) {
 
 
 $(document).ready(function() {
+  // form object
   let form_obj = "form";
+  // result container
   let result_obj = ".scriptcontainer"
 
+  // when the text intputs change
   $(form_obj + " input").change(function() {
     let all_filled = true;
+    // check if both are non empty
     $(form_obj + ' input[type="text"]').each(function() {
       if (!$(this).val()) {
         all_filled = false;
       }
     });
 
+    // if both are filles we enable the "generate code" button
     if (all_filled) {
       $(form_obj + " #generate").each(function() {
         $(this).attr("disabled", false);
@@ -78,17 +90,26 @@ $(document).ready(function() {
   })
 
   $(form_obj + " #generate").click(function() {
+    // nuber of iterations
     let iterations = $(form_obj + " #iterations").val();
+    // obfuscate code checkbox
     let obfuscate = $(form_obj + " #obfuscator").prop('checked');
+    // hide variables checkbox
     let hide_variables = $(form_obj + " #hidevariables").prop('checked');
+    // strip newlines checkbox
     let strip_newlines = $(form_obj + " #striplines").prop('checked');
 
+    // encode email
     encoded_email = encode($(form_obj + " #email").val(), iterations);
+    // encode container
     encoded_container = encode($(form_obj + " #container").val(), iterations);
 
+    // create code
     js_code = generateCode(encoded_container, encoded_email, iterations, obfuscate, hide_variables, strip_newlines);
+    // add it to result container
     $(result_obj).html(js_code);
 
+    // add download link to button
     let download = generateDowload(js_code, form_obj + " #download");
     $(form_obj + " #download").attr({"disabled": false});
     $(form_obj + " #download").children().attr({"href": download, "download": "hide-email.js"});
