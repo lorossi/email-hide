c9dd3df0122838a17373aa98e8a637c5852899a9
lorossi <lo.rossi@live.com>
2020-07-05 19:22:17 +0200

    added codetemplate.js file

---
+++ js/codetemplate.js
@@ -0,0 +1,22 @@
+function codeTemplate(options) {
+  let newl, tab, js_code;
+  
+  if (options.strip_newlines) {
+    newl = "";
+    tab = "";
+  } else {
+    newl = "\n";
+    tab = "   ";
+  }
+
+  if (options.max_iterations == 0) {
+    js_code = `window.onload=function ${options.function_name}(){${newl}${tab}document.querySelector("${options.encoded_container}").innerHTML="${options.encoded_email}";${newl}}`;
+  }
+  else if (options.max_iterations == 1) {
+    js_code = `window.onload=function ${options.function_name}(){${newl}${tab}let ${options.container_variable}=atob("${options.encoded_container}");${newl}${tab}let ${options.email_variable}=atob("${options.encoded_email}");${newl}${tab}document.querySelector(${options.container_variable}).innerHTML=${options.email_variable};${newl}}`;
+  } else {
+    js_code = `window.onload=function ${options.function_name}(){${newl}${tab}let ${options.container_variable}="${options.encoded_container}";${newl}${tab}let ${options.email_variable}="${options.encoded_email}";${newl}${tab}for(let ${options.index_variable}=0;${options.index_variable}<${options.max_iterations};${options.index_variable}++){${newl}${tab}${tab}${options.container_variable}=atob(${options.container_variable});${newl}${tab}${tab}${options.email_variable}=atob(${options.email_variable});${newl}${tab}}${newl}${tab}document.querySelector(${options.container_variable}).innerHTML=${options.email_variable};${newl}}`;
+  }
+
+  return js_code;
+}
